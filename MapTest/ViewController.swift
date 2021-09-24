afa04cdaa330ceab71692e6254289bdaf5a26c08
shakhzod <shakhzodurinboev@gmail.com>
2021-09-24 12:13:37 +0500

    Initial Commit

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by shakhzodurinboev on 24/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
